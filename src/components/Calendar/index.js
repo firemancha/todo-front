2db11b1465f40c9bf03b225c09968c014a256544
firemancha <vaite714@gmail.com>
2022-04-22 21:27:29 +0900

    캘린더 헤더 작업

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -2,11 +2,27 @@ import "./index.css";
 import CalendarHeader from "../CalendarHeader";
 import CalendarFooter from "../CalendarFooter";
 import CalendarContent from "../CalendarContent";
+import { useState, useEffect } from "react";
 
 const CalendarTemplate = () => {
+  const [today, setToday] = useState("");
+
+  useEffect(() => {
+    const now = new Date();
+    let tmp = {};
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1;
+    const date = now.getDate();
+    tmp.year = year;
+    tmp.month = month;
+    tmp.date = date;
+
+    setToday(tmp);
+  }, []);
+
   return (
     <div className="calendar-template">
-      <CalendarHeader />
+      <CalendarHeader today={today} />
       <CalendarContent />
       <CalendarFooter />
     </div>
--- src/components/CalendarHeader/index.js
+++ src/components/CalendarHeader/index.js
@@ -1,10 +1,25 @@
 import "./index.css";
 
-const CalendarHeader = () => {
+const CalendarHeader = (props) => {
+  const { today } = props;
   return (
     <div className="calendar-header">
-      <div>연도</div>
-      <div>버튼</div>
+      <div className="calendar-header-title">
+        <div className="calendar-header-title-month">
+          {today.year} .{" "}
+          {today.month < 10 ? "0" + today.month.toString() : today.month}
+        </div>
+        <div className="calendar-header-title-button">{today.month}</div>
+      </div>
+      <div className="calendar-header-day">
+        <div className="calendar-header-day-sun">일</div>
+        <div className="calendar-header-day-week">월</div>
+        <div className="calendar-header-day-week">화</div>
+        <div className="calendar-header-day-week">수</div>
+        <div className="calendar-header-day-week">목</div>
+        <div className="calendar-header-day-week">금</div>
+        <div className="calendar-header-day-sat">토</div>
+      </div>
     </div>
   );
 };
